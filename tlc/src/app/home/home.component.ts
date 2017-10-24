2ea73f24bbb88c1a51bf1d737de2e2fd698a8725
Logan Crewss <LCrewss@ymail.com>
2017-10-24 09:58:57 -0600

    add jQuery

--- tlc/src/app/home/home.component.ts
+++ tlc/src/app/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+
+$('#elemId').width();
+
+
 
 @Component({
   selector: 'app-home',
@@ -10,6 +16,8 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('ul.tabs').tabs();
+
   }
 
 }
